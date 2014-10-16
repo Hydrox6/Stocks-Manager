d205c41cc1477446964b201310582f73b5b043bc
Hydrox6 <ikada@outlook.com>
2014-10-17 00:47:07 +0100

    A lot of changes to the grapher
    Added an interpolate function to fix axes bugs
    Fixed a bug where y values were not being considered for max/min y
    Fixed the calculation for xaxisy and yaxisx
    Fixed and finished the axes drawing loop
    Added a plotting loop
    Added a "dot to dot" point joining method
    Added a stub for the line of best fit joining method
    Added some test code, commented out by default

--- grapher.py
+++ grapher.py
@@ -3,17 +3,27 @@ import math as maths
 
 
 
+def interpolate(r1,r2,f1,f2,rd):
+    r2 -= r1
+    rd -= r1
+    r1 -= r1
+    pr = float(rd)/float(r2-r1)
+    pf = pr*(f2-f1)
+    print r1,r2,rd,pr,pf
+    return f1 +pf
+
 
 def plot(canvas,points,axis,title,mode):
-    height = winfo_height()
-    width = winfo_width()
-    draww = width-20
-    drawh = height-20
+    margin = 40
+    height = canvas.winfo_height()
+    width = canvas.winfo_width()
+    draww = width-margin*2
+    drawh = height-margin*2
     maxx = {"+y":0.0,"-y":0.0,"+x":0.0,"-x":0.0}
     for x in points:
         if   x[0] < maxx["-x"]:maxx["-x"] = maths.floor(x[0])
         elif x[0] > maxx["+x"]:maxx["+x"] = maths.ceil(x[0])
-        elif x[1] < maxx["-y"]:maxx["-y"] = maths.floor(x[1])
+        if   x[1] < maxx["-y"]:maxx["-y"] = maths.floor(x[1])
         elif x[1] > maxx["+y"]:maxx["+y"] = maths.ceil(x[1])
     rangex = maxx["+x"]-maxx["-x"]
     rangey = maxx["+y"]-maxx["-y"]
@@ -21,18 +31,53 @@ def plot(canvas,points,axis,title,mode):
     dpy = rangey/float(height)
     ppx = 1/dpx
     ppy = 1/dpy
-    fourpx = rangex*0.04
-    fourpy = rangey*0.04
-    xaxisy = maxx["+y"]-maxx["+y"]/dpy
-    yaxisx = maxx["+x"]-maxx["+x"]/dpx
-    canvas.create_line(10,xaxisy,width-10,xaxisy)#x axis
-    canvas.create_line(yaxisx,10,yaxisx,height-10)#y axis
+    fourpx = rangex/25
+    fourpy = rangey/25
+    xaxisy = height-margin-maxx["-y"]*ppy
+    yaxisx = 0+margin-maxx["-x"]*ppx
+    canvas.create_line(margin,xaxisy,width-margin,xaxisy,width=2)#x axis
+    canvas.create_line(yaxisx,margin,yaxisx,height-margin,width=2)#y axis
     for x in range(0,26):
-        posx = maxx["-x"]+fourpx*x
-        if not posx == 0:
-            canvas.create_line(posx,yaxisx,posx,yaxisx-3)
-            #text
-        posy = maxx["-y"]+fourpy*x
-        if not posy == 0:
-            canvas.create_line(xaxisy,posy,xaxisy-3,posy)
-            #text
+        #X Axis Markers
+        rawx = maxx["-x"]+fourpx*x
+        print rawx
+        posx = interpolate(maxx["-x"],maxx["+x"],margin,width-margin,rawx)
+        print posx
+        if not rawx == 0:
+            canvas.create_line(posx,xaxisy,posx,xaxisy+3,width=1.5)
+            canvas.create_text(posx,xaxisy+10,text=str(rawx))
+        #Y Axis Markers
+        rawy = maxx["-y"]+fourpy*x
+        posy = interpolate(maxx["-y"],maxx["+y"],height-margin,margin,rawy)
+        if not rawy == 0:
+            canvas.create_line(yaxisx,posy,yaxisx-3,posy,width=1.5)
+            canvas.create_text(yaxisx-10,posy,text=str(rawy),anchor="e")
+    
+    for p in points:
+        posx = interpolate(maxx["-x"],maxx["+x"],margin,width-margin,p[0])
+        posy = interpolate(maxx["-y"],maxx["+y"],height-margin,margin,p[1])
+        canvas.create_line(posx-3,posy-3,posx+3,posy+3,width=1.75)
+        canvas.create_line(posx-3,posy+3,posx+3,posy-3,width=1.75)
+        canvas.create_oval(posx+3,posy+2,posx+3,posy+2,width=1)#fixes corner pixel
+        canvas.create_oval(posx+3,posy-3,posx+3,posy-3,width=1)#fixes corner pixel
+
+    if mode.lower() == "join":
+        for x in range(1,len(points)):
+            posx1 = interpolate(maxx["-x"],maxx["+x"],margin,width-margin,points[x][0])
+            posy1 = interpolate(maxx["-y"],maxx["+y"],height-margin,margin,points[x][1])
+            posx2 = interpolate(maxx["-x"],maxx["+x"],margin,width-margin,points[x-1][0])
+            posy2 = interpolate(maxx["-y"],maxx["+y"],height-margin,margin,points[x-1][1])
+            canvas.create_line(posx1,posy1,posx2,posy2)
+
+    elif mode.lower() == "best fit":pass
+        
+
+#test code         
+"""
+root = Tk()
+point = [[-1,0],[1,2],[3,7],[6,2],[10,10]]
+c = Canvas(width=800,height=800)
+c.pack()
+Button(root,text="go",command=lambda:plot(c,point,["x","y"],"Thing","join")).pack()
+root.mainloop()
+"""
