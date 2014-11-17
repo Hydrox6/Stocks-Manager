caddb52b922afd3f296b25fc4d30e1883bd5f8ab
Hydrox6 <ikada@outlook.com>
2014-11-17 15:17:08 +0000

    The other half of the commit
    grumble school grumble bastards grumble
    
    get is done!

--- get.py
+++ get.py
@@ -1,21 +1,56 @@
+# -*- coding: cp1252 -*-
 import urllib2
 
 codes = ["ASBE","CAMB","COG","VRP","NG."]
-prices = {}
+#prices = {}
 
+currencies = {"EUR":"EUR","GBX":"GBP"}
+signs = {"EUR","€","GBP":"£","USD":"$"}
 
-base = "http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/prices-search/stock-prices-search.html?nameCode={name}&page=1"
-for x in codes:
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-    u = opener.open(base.format(name=x)).read()
+bases = {"conv":"http://www.xe.com/currencyconverter/convert/?Amount={amount}&From={fom}&To={to}",
+         "check":"http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/prices-search/stock-prices-search.html?nameCode={name}&page=1"}
+
+class Stock:
+    #private String code
+    #private String name
+    #private String currency
+    #private String price
+
+    def add(self,tag,data):setattr(self,tag,data)
+
+    def curconv(self,to):
+        u = opener.open(bases["conv"].format(amount=self.price,fom=currencies[self.currency],to=to)).read()
+        u = u.split('<tr class="uccRes">')[1].split("</tr>")[0].split("</td>")[2]
+        u = u.split(">")[1].split("&")[0]
+        self.currency = to
+        self.price = u
+
+    def expand():return [self.code,self.name,signs[self.currency]+self.price]
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]  
+        
+
+tags = ["code","name","currency","price"]
+
+def get(code):
+    #for x in codes:
+    u = opener.open(bases["check"].format(name=x)).read()
     u = u.split('class="table_dati"')[1].split("</table>")[0].split("<tbody>")[1].split("</tbody>")[0]
     u = u.split("</td>")
-    for y in range(0,len(u)):
+    s = Stock()
+    for y in range(0,4):
+        if y == 1:
+            d = u[y].split(">")[-2][:-3].strip()
+        else:
+            d = u[y].split(">")[-1].rstrip()
         if y == 0:
-            t = u[y].split(">")[-1].rstrip()
-            if t != x:
+            if d != x:
                 break
-        if y == 3:
-            p = float(u[y].split(">")[-1].rstrip())
-            prices[x] = p
+        else:
+            s.add(tags[y],d)
+    
+    return s
+
+
+sss["CAMB"].curconv("EUR")
